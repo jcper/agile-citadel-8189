52867f396f9e0fe6c732f0453af6139ddf978150
jcper <informaticajcper@gmail.com>
2017-04-05 23:36:05 +0200

    actualizacion log

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const SocketServer = require('ws').Server;
 const path = require('path');
 var fs = require('fs');
 var alarma;
+var TiempoEspera=600000;
+var FinalizaTiempo;
+var dia;
 var Fecha=new Date().toTimeString();
 
 const PORT = process.env.PORT || 3001;
@@ -20,36 +23,25 @@ wss.on('connection', function(ws) {
 	  console.log('client conected');
 
     ws.on('message', function(message) {
-        console.log('received: %s', message);
+        dia=new Date();
+        FinalizaTiempo=dia + TiempoEspera
         alarma=message;
        if(alarma.conexion!==undefined){
-         var dia=new Date();
-        data='-Usuario: '+alarma.name+'-Conexion'+alarma.conexion+'-Date'+d.toUTCString()+'-Ubicacion'+alarma.ip+'/n ';
-        fs.appendFile('serverlog.txt',data, function(err) {
-       if( err ){
-        console.log( err );
-          };
-         });
+        dataconexion='-Usuario: '+alarma.name+'-Conexion'+alarma.conexion+'-Date'+dia.toUTCString()+'-Ubicacion'+alarma.ip;
+        console.log('[conexion]: %s', dataconexion);
        }
 
-       if(alarma.alarma!==undefined){
-         var dia=new Date();
-        data='-Usuario: '+alarma.name+'-Alarma'+alarma.alarma+'-Date'+d.toUTCString()+'-Ubicacion'+alarma.ip+'/n ';
-        fs.appendFile('serverlog.txt',data, function(err) {
-       if( err ){
-        console.log( err );
-          };
-        });
+        if(alarma.alarma!==undefined && dia >=FinalizaTiempo){
+        dataalarma='-Usuario: '+alarma.name+'-Conexion'+alarma.conexion+'-Date'+dia.toUTCString()+'-Ubicacion'+alarma.ip;
+        console.log('[alarma]: %s', datalarma);
+        FinalizaTiempo=0; 
+       }
+
       
-      }
         if(alarma.ErrorEthernet!==undefined){
          var dia=new Date();
-        data='-Usuario: '+alarma.name+'-Alarma'+alarma.ErrorEthernet+'-Date'+d.toUTCString()+'-Ubicacion'+alarma.ip+'/n ';
-        fs.appendFile('serverlog.txt',data, function(err) {
-       if( err ){
-        console.log( err );
-          };
-        });
+        dataerrorethernet='-Usuario: '+alarma.name+'-Alarma:ErrorEthernet'+alarma.ErrorEthernet+'-Date'+dia.toUTCString()+'-Ubicacion'+alarma.ip;
+        console.log('[ErrorEthernet]: %s', dataerrorethernet);
        }
       });
 
